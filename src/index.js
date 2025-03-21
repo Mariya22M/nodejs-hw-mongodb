65ae023fb6d7255d247fd1c39cdca7e6faa59c49
Mariya22M <lutsakmaria14@gmail.com>
2025-03-21 15:21:26 +0200

    Опис змін або виправлень

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+
 const bootstrap = async () => {
   try {
     // ініціалізація підключення до MongoDB
     await initMongoConnection();
-    logger.info('MongoDB connection successfully established.');
+    console.log('MongoDB connection successfully established.');
 
     // налаштування сервера
     setupServer();
-    logger.info('Server setup completed and running.');
+    console.log('Server setup completed and running.');
   } catch (error) {
-    logger.error(`Error during bootstrap: ${error.message}`);
+    console.error(`Error during bootstrap: ${error.message}`);
     process.exit(1); // Завершення процесу при помилці
   }
 };
