adcbfaa1c16777ea45d69f61a757917b702a6e0d
Mariya22M <lutsakmaria14@gmail.com>
2025-04-07 00:02:02 +0300

    remove ctrlwqrapper

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ export const setupServer = () => {
 
   app.use('/uploads', express.static(UPLOAD_DIR));
 
-  app.use('/api-docs', ctrlWrapper(swaggerDocs));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
